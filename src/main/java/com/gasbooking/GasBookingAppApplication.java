5583bd252743e9fa9f33fb3455ad77f02c7849ad
Lewis <tracy.lewis@capgemini.com>
2021-11-19 17:42:11 +0530

    initial commit

---
+++ src/main/java/com/gasbooking/GasBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.gasbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasbooking/GasBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
